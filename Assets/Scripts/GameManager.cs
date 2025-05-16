4d4772063304694f5c68f1d036c29d368c0defd1
reda0206 <cjones7@ltu.edu>
2025-05-16 15:37:01 -0400

    Added Pause Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager Instance;
+    public GameObject pauseMenuUi;
+    public bool isPaused = false;
+    private List<AudioSource> audioSources = new List<AudioSource>();
+    public List<AudioSource> excludedAudioSources = new List<AudioSource>();
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        pauseMenuUi.SetActive(false);
+        ResumeAudio();
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        pauseMenuUi.SetActive(true);
+        PauseAudio();
+        Time.timeScale = 0f;
+    }
+
+    public void QuitToMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    public void PauseAudio()
+    {
+        foreach (AudioSource audio in FindObjectsOfType<AudioSource>())
+        {
+            if (audio.isPlaying && !excludedAudioSources.Contains(audio))
+            {
+                audio.Pause();
+                audioSources.Add(audio);
+            }
+        }
+    }
+
+    public void ResumeAudio()
+    {
+        for (int i = audioSources.Count - 1; i >= 0; i--)
+        {
+            if (audioSources[i])
+            {
+                audioSources[i].UnPause();
+                audioSources.RemoveAt(i);
+            }
+        }
     }
 }
